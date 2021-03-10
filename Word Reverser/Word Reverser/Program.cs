ecc9358b25b413c2b2e9b59b7a7d68736a0c13ad
LEINADALIEN <79170882+leinadalien@users.noreply.github.com>
2021-03-11 01:57:51 +0300

    Add files via upload

---
+++ Word Reverser/Word Reverser/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Word_Reverser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введи строку");
+            string Line = Console.ReadLine();
+            String[] Words = Line.Split(new char[] { ' ' });
+            for(int i = 0; i < Words.Length/2; i++)
+            {
+                Line = Words[i];
+                Words[i] = Words[Words.Length - i - 1];
+                Words[Words.Length - i - 1] = Line;
+            }
+            Line = String.Join(" ", Words);
+            Console.WriteLine(Line);
+        }
+    }
+}
