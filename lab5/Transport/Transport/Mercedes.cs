ac4a9910c85a3bfee812f28be9c1f5e166421009
LEINADALIEN <79170882+leinadalien@users.noreply.github.com>
2021-05-14 15:21:38 +0300

    Add files via upload

---
+++ lab5/Transport/Transport/Mercedes.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Transport
+{
+    class Mercedes : Car
+    {
+        public enum MercedesModel
+        {
+            A_Class, B_Class, C_Class, E_Class, S_Class, V_Class, GLE, SLS, SLK, GLK, SL, GL, Vito, 
+        }
+        private MercedesModel Model = MercedesModel.A_Class;
+        private static string ToString(MercedesModel Model)
+        {
+            return Model.ToString().Replace('_', ' ');
+        }
+
+        protected override void PrintTransportInfo()
+        {
+            base.PrintTransportInfo();
+            Console.WriteLine("Car Brand: Mercedes");
+            Console.WriteLine("Model: " + ToString(Model));
+        }
+        public Mercedes() : base("Germany")
+        {
+            if (RegistrationNumber != "")
+            {
+                TransportInfo = "Mercedes " + ToString(Model) + "[" + RegistrationNumber + "]";
+            }
+            else
+            {
+                TransportInfo = "Mercedes " + ToString(Model) + " without registration number";
+            }
+        }
+        public Mercedes(ConsoleColor Color) : base("Germany", Color)
+        {
+            if (RegistrationNumber != "")
+            {
+                TransportInfo = "Mercedes " + ToString(Model) + "[" + RegistrationNumber + "]";
+            }
+            else
+            {
+                TransportInfo = "Mercedes " + ToString(Model) + " without registration number";
+            }
+        }
+        public Mercedes(MercedesModel Model, ConsoleColor Color) : base("Germany", Color)
+        {
+            this.Model = Model;
+            if (RegistrationNumber != "")
+            {
+                TransportInfo = "Mercedes " + ToString(Model) + "[" + RegistrationNumber + "]";
+            }
+            else
+            {
+                TransportInfo = "Mercedes " + ToString(Model) + " without registration number";
+            }
+        }
+        public Mercedes(MercedesModel Model) : base("Germany")
+        {
+
+            this.Model = Model;
+            if (RegistrationNumber != "")
+            {
+                TransportInfo = "Mercedes " + ToString(Model) + "[" + RegistrationNumber + "]";
+            }
+            else
+            {
+                TransportInfo = "Mercedes " + ToString(Model) + " without registration number";
+            }
+        }
+        public override void SetRegistrationNumber(string RegistrationNumber)
+        {
+            this.RegistrationNumber = RegistrationNumber;
+            TransportInfo = "Mercedes " + ToString(Model) + "[" + RegistrationNumber + "]";
+        }
+        public override void TakeOffRegistrationNumber()
+        {
+            RegistrationNumber = "";
+            TransportInfo = "Mercedes " + ToString(Model) + " without registration number";
+        }
+        //Getters
+        public MercedesModel GetModel()
+        {
+            return Model;
+        }
+    }
+}
---
+++ lab5/Transport/Transport/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Transport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Audi a = new Audi();
+            
+            a.Refuel(5);
+            a.SetRegistrationNumber("7856 IM-1");
+            a.PrintInfo();
+            Console.WriteLine();
+            Transport b = new Mercedes();
+            b.PrintInfo();
+        }
+    }
+}
