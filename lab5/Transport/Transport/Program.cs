35ac07002768a7448a0ed9dbb6d6a1c90ef035ad
LEINADALIEN <79170882+leinadalien@users.noreply.github.com>
2021-05-14 13:00:38 +0300

    Add files via upload

---
+++ lab5/Transport/Transport/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Transport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Audi a = new Audi();
+            a.Refuel(5);
+            a.SetRegistrationNumber("7856 IM-1");
+            a.PrintInfo();
+            a.Use();
+            Volkswagen b = new Volkswagen();
+            b.Refuel(10);
+            b.AddModelInfo("b5");
+            b.SetRegistrationNumber("8234 IX-1");
+            b.PrintInfo();
+            b.Use();
+            Mercedes c = new Mercedes();
+            c.Refuel();
+            c.PrintInfo();
+        }
+    }
+}
